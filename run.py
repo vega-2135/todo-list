1024beb3093a9e62fadef77ccbad116ad8c794b3
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-01-25 10:09:04 +0100

    Add get_user_input and main functions

--- run.py
+++ run.py
@@ -1,3 +1,47 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('credentials.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('todo-list')
+
+todo = SHEET.worksheet("todo")
+message1 = f"Choose one of the following:\n"
+message2 = f"(1) Create a new to-do list\n(2) Open to-do lists\n(3) Help\n(4) Exit\n"
+
+def get_user_input():
+    """ 
+    Gets option from user and validates that the input is an integer between 1 and 4,
+    if the input does not follows these constrains, then a ValueError is raise 
+    """
+    while True:
+        
+        try:
+            user_input = int(input("Please write your option here and press Enter to confirm your selection:\n"))
+            if user_input >= 1 or user_input <= 4:
+                return user_input
+                break
+
+            else:
+                raise ValueError()
+            
+        except ValueError:
+            print("Invalid Answer")
+
+
+
+def main():
+
+    print("Welcome to My TO-DO List!\n")
+    print(f"{message1}\n{message2}")
+    user_answer = get_user_input()
+    print(user_answer)
+
+main()
